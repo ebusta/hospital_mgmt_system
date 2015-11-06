d6d29e984fd49788815f2c4f78848b7db8d655db
ebusta <erik.smistad.music@gmail.com>
2015-11-06 10:46:58 -0700

    Create MagicalInfirmarySystem.java

---
+++ MagicalInfirmarySystem.java
@@ -0,0 +1,28 @@
+/**
+ * This is where the user will begin using the whole Infirmary program.
+ * @author Erik Smistad
+ * @version 1.0
+ * Last Modified: March 16, 2015
+ */
+public class MagicalInfirmarySystem
+{
+    /**
+     * The starting point of this whole sexy thing
+     */    
+    public static void main(String[] args)
+    {
+        Run run = new Run();
+        run.start();
+    }
+    
+    /* To Do:
+     * 4. Jar Files/Packages?
+     */
+    
+    /* Questions for Kidney
+     * 2. In Login, there are several similar username searches. Do they all need to be there?
+     *    Does it matter that the login version cares about capitalization, but the admin 
+     *    one doesn't? 
+     * 3. 
+     */
+}
